ee4529164c3a635750c287e7513024725b21cd5a
Seth <seth@salukicorporation.com>
2021-04-13 15:49:46 +0100

    Added layers and buttons to args

--- main.py
+++ main.py
@@ -4,13 +4,26 @@ import random
 import asyncio
 import time
 import subprocess
+
+
+
+#Global Settings
 prefix = ";"
 version = "v0.1"
+colour = 0x0ccfaf
+
 bot = commands.Bot(command_prefix=prefix)
 
 
+
 # Map Generation Variables
-allowedArgs = ["","sepia","greyscale","dingy","tint"]
+allowedButtons = ["sepia","grayscale","dingy","tint"]
+allowedLayers = ["Texture","Height","Biomes","Cells",
+                 "Grid","Coordinates","Compass","Rivers","Relief",
+                 "Religions","Cultures","States","Provicnes","Zones",
+                 "Borders","Routes","Temp","Ice","Population",
+                 "Prec","Emblems","Labels","Icons","Military",
+                 "Markers","Rulers","ScaleBar"]
 
 @bot.event
 async def on_ready():
@@ -23,17 +36,26 @@ async def status(ctx):
     shard = "No shard detected" if str(bot.shard_id) == "None" else  str(bot.shard_id) + "/" + str(bot.shard_count)
 
 
-    embed=discord.Embed(title="Status", description="Bot status information", color=0x06bc5e)
+    embed=discord.Embed(title="Status", description="Bot status information", color=colour)
     embed.add_field(name="Latency", value=latency, inline=True)
     embed.add_field(name="Shard", value=shard, inline=True)
     embed.add_field(name="Version", value=version, inline=True)
     await ctx.send(embed=embed)
 
 @bot.command()
-async def map(ctx, arg = ""):
+async def map(ctx, *args):
     async with ctx.typing():
-        if arg in allowedArgs:
-            subprocess.run("node web.js "+arg, shell=True)      
+        invalidArg = False
+        argsToGo = ""
+        for arg in args:
+            if arg.lower() in allowedButtons:
+                argsToGo += "b"+arg.lower()+" "
+            elif arg[0].upper()+arg[1:].lower() in allowedLayers:
+                argsToGo += "l"+arg[0].upper()+arg[1:].lower()+" "
+            else:
+                invalidArg = True
+        if not invalidArg:
+            subprocess.run("node web.js "+argsToGo, shell=True)      
             with open("cache/map.png", "rb") as fh:
                 f = discord.File(fh, filename="map.png")
             await ctx.send(file=f)
@@ -42,8 +64,18 @@ async def map(ctx, arg = ""):
 
 @bot.command()
 async def args(ctx):
-    embed=discord.Embed(title="Allowed Arguments", color=0x0ccfaf)
-    embed.add_field(name="Arguments", value=allowedArgs, inline=False)
+    embed=discord.Embed(title="Allowed Arguments", color=colour)
+    buttons = ""
+    for button in allowedButtons:
+        buttons+=button+", "
+    buttons = buttons[:-2]
+
+    layers = ""
+    for layer in allowedLayers:
+        layers+=layer+", "
+    layers = layers[:-2]
+    embed.add_field(name="Filters", value=buttons, inline=False)
+    embed.add_field(name="Layers", value=layers, inline=False)
     embed.set_footer(text="Use any of these after the map command")
     await ctx.send(embed=embed)
 
--- web.js
+++ web.js
@@ -1,14 +1,19 @@
 const puppeteer = require('puppeteer');
 var myArgs = process.argv.slice(2);
 
-
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://azgaar.github.io/Fantasy-Map-Generator');
   await new Promise(r => setTimeout(r, 2000));
   for (let i= 0; i < myArgs.length; i++){
-    await page.$eval('button[id='+myArgs[i]+']', el => el.click());
+    if (myArgs[i].charAt(0) == "b") {
+    await page.$eval('button[id='+myArgs[i].substring(1)+']', el => el.click());
+    }
+    else if (myArgs[i].charAt(0) == "l") {
+      await page.$eval('#toggle'+myArgs[i].substring(1), el => el.click());
+      }
+  
   }
   await page.screenshot({ path: 'cache/map.png' });
   await browser.close();
