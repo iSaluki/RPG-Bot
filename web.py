61c266855b2b0838212d7b13244aabbd00275ebf
Seth <seth@salukicorporation.com>
2021-04-13 12:52:35 +0100

    Begin working on porting to Python

--- web.py
+++ web.py
@@ -1,10 +1,14 @@
 import asyncio
+import time
 from pyppeteer import launch
 
 async def main():
     browser = await launch()
     page = await browser.newPage()
-    await page.goto('http://google.com')
+    await page.goto('https://azgaar.github.io/Fantasy-Map-Generator')
+    time.sleep(10)
+    await page.click("button.ui-button.ui-corner-all.ui-widget")
+    time.sleep(5)
     await page.screenshot({'path': 'example.png'})
     await browser.close()
 
